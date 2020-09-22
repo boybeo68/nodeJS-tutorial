737517797077c547ada1cba6865d9042ee8ee2b5
nguyen thac tung <tung.nguyen.thac@ntq-solution.com.vn>
2020-09-22 17:42:45 +0700

    done page

--- Phone-app/controller/products.controller.js
+++ Phone-app/controller/products.controller.js
@@ -1,9 +1,15 @@
 const shortid = require("shortid");
 // config low db
 const db = require("../db");
-const products = db.get("product").value().slice(0, 6);
+const products = db.get("product").value();
 module.exports.getProducts = (req, res, nex) => {
+  const page = parseInt(req.query.page) || 1;
+  const perPage = 8;
+  const start = (page - 1) * perPage;
+  const end = page * perPage;
+  const totalPage = Math.round(products.length / perPage);
   return res.render("products/products", {
-    products: products,
+    products: products.slice(start, end),
+    totalPage: totalPage,
   });
 };
